434c035b410c17d007efec3e0339760cc58212a0
Vergz <vergarigiacomo20@gmail.com>
2022-04-16 17:25:54 +0200

    .

--- src/main/Pannello_gioco.java
+++ src/main/Pannello_gioco.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class Pannello_gioco extends JPanel{
+public class Pannello_gioco extends JPanel implements Runnable{
   //IMPOSTAZIONI SCHERMO
 	final int originalTileSize=16;
 	final int scale=3;
@@ -16,9 +16,27 @@ public class Pannello_gioco extends JPanel{
 	final int screenWidth=tileSize*maxScreenCol;
 	final int screenHeight=tileSize*maxScreenRow;
 	
+	Thread gameThread;
+	
 	public Pannello_gioco() {
 		this.setPreferredSize(new Dimension(screenWidth,screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+	}
+
+	public void startGameThread()
+	{
+		gameThread=new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+      
+		while(gameThread!=null) {
+
+		}
+		
+		
 	}
 }
