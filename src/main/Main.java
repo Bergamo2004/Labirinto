64ff37dba065a121f32207d6cece813fe04d77b0
Vergz <vergarigiacomo20@gmail.com>
2022-04-16 17:12:22 +0200

    prova implementazione base finestra

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,22 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("ciao");
+		JFrame window =new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Prova Labirinto");
+		
+		Pannello_gioco pannello=new Pannello_gioco();
+		window.add(pannello);
+		
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
 
 	}
 
