91df75b8c5cbef7faf0a1b0beae68e2b74cdb901
Vergz <vergarigiacomo20@gmail.com>
2022-05-02 14:40:47 +0200

    Update Editor.java

--- src/controllo/Editor.java
+++ src/controllo/Editor.java
@@ -1,11 +1,14 @@
 package controllo;
 
+import java.awt.Color;
+import java.awt.Frame;
+
 import javax.swing.*;    
 import javax.swing.event.*;  
 public class Editor {    
       public static void main(String[] a) {  
             JFrame f = new JFrame("prova tabella");          
-                            final JTable jt=new JTable(50,50);    
+                            final  JTable jt=new JTable(50,50);    
             jt.setCellSelectionEnabled(true);  
             ListSelectionModel select= jt.getSelectionModel();  
             select.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);  
@@ -16,7 +19,12 @@ public class Editor {
                 int[] columns = jt.getSelectedColumns();  
                 for (int i = 0; i < row.length; i++) {  
                   for (int j = 0; j < columns.length; j++) {  
-                    Data = (String) jt.getValueAt(row[i], columns[j]);  
+                   Data = (String) jt.getValueAt(row[i], columns[j]);  
+                    
+                    Color rosso=new Color(255,0,0);
+                    jt.setBackground(rosso);
+                    
+                    
                   } }  
                 System.out.println("L'elemento selezionato: " + Data);    
               }       
