d30ea62760ef196787209ce7cc3a0b65fbe16115
Vergz <vergarigiacomo20@gmail.com>
2022-05-03 09:53:45 +0200

    Update Editor.java

--- src/controllo/Editor.java
+++ src/controllo/Editor.java
@@ -7,8 +7,10 @@ import java.awt.Frame;
 import javax.swing.*;    
 import javax.swing.event.*;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;  
-public class Editor {    
+import javax.swing.table.DefaultTableModel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;  
+public class Editor {
       public static void main(String[] a) {  
             JFrame f = new JFrame("prova tabella");          
                             final  JTable jt=new JTable(50,50);    
@@ -32,8 +34,37 @@ public class Editor {
                 changeTable(jt,columns.length);
               }       
             });  
+            f.getContentPane().setLayout(new BoxLayout(f.getContentPane(), BoxLayout.X_AXIS));
             JScrollPane sp=new JScrollPane(jt);    
-            f.add(sp);  
+            f.getContentPane().add(sp);
+            
+            JPanel panel = new JPanel();
+            f.getContentPane().add(panel);
+            panel.setLayout(null);
+            
+            JButton btnNewButton = new JButton("New button");
+            btnNewButton.setBounds(59, 60, 89, 23);
+            panel.add(btnNewButton);
+            
+            JButton btnNewButton_1 = new JButton("New button");
+            btnNewButton_1.setBounds(59, 147, 89, 23);
+            panel.add(btnNewButton_1);
+            
+            JButton btnNewButton_2 = new JButton("New button");
+            btnNewButton_2.setBounds(59, 226, 89, 23);
+            panel.add(btnNewButton_2);
+            
+            JButton btnNewButton_3 = new JButton("New button");
+            btnNewButton_3.setBounds(59, 308, 89, 23);
+            panel.add(btnNewButton_3);
+            
+            JButton btnNewButton_4 = new JButton("New button");
+            btnNewButton_4.setBounds(59, 394, 89, 23);
+            panel.add(btnNewButton_4);
+            
+            JButton btnNewButton_5 = new JButton("New button");
+            btnNewButton_5.setBounds(59, 502, 89, 23);
+            panel.add(btnNewButton_5);
             f.setSize(900, 865);  
             f.setVisible(true);  
           }
@@ -107,3 +138,34 @@ class MyTableCellRenderer extends DefaultTableCellRenderer {
         });
     }
     */
+
+
+
+
+/*
+public void loading() {
+try {
+    String[]title = {"First Name","Last Name","Picture"};
+    String sql="select * from users";
+    model = new DefaultTableModel(null,title){
+        @Override
+        public Class<?> getColumnClass(int column) {
+            if (column==2) return ImageIcon.class;
+            return Object.class;
+        }
+    }
+    st = conn.createStatement();
+    ResultSet rs = st.executeQuery(sql);
+    Object[]fila = new Object[4];
+    while(rs.next()){
+        fila[0] = rs.getString("fna");
+        fila[1] = rs.getString("lna");
+        fila[2] = new ImageIcon(rs.getBytes("pic"));            
+        model.addRow(fila);
+    }
+    tbl.setModel(model);
+}
+catch (SQLException e) {
+    JOptionPane.showMessageDialog(null, e.getMessage());
+}
+}*/
