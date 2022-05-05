48ebec545bca5b3957eac72e372e5f8126e2dea1
Lorenzo <103416334+Ceschissimo04@users.noreply.github.com>
2022-05-05 19:23:43 +0200

    Create OggDoor.java

---
+++ src/oggetto/OggDoor.java
@@ -0,0 +1,18 @@
+package oggetto;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class OggDoor extends SuperOggetto{
+	
+	public OggDoor() {
+		nome = "porta";
+    collision = true;
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream("/oggetti/porta.png"));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
